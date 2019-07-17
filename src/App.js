fd84bde5be4eabe6a710dbdbddd6796ae5a95016
lawrence <lawrence@factual.com>
2019-07-17 16:09:20 -0700

    TABLES!!!!!!

--- src/App.js
+++ src/App.js
@@ -23,15 +23,151 @@ const { Search } = Input;
 const { Header, Footer, Sider, Content } = Layout;
 
 let segments = require("./segments.json");
-console.log(segments);
 
-const country = (
-  <Menu>
-    <Menu.Item key="1">Country 1</Menu.Item>
-    <Menu.Item key="2">Country 2</Menu.Item>
-    <Menu.Item key="3">Country 3</Menu.Item>
-  </Menu>
-);
+const tags = [
+  { text: "Advertising and Marketing", value: "Advertising and Marketing" },
+  { text: "Age", value: "Age" },
+  { text: "Amusement and Theme Parks", value: "Amusement and Theme Parks" },
+  { text: "Arts, Crafts and Fabric", value: "Arts, Crafts and Fabric" },
+  { text: "Automotive", value: "Automotive" },
+  { text: "Baby and Children's Goods", value: "Baby and Children's Goods" },
+  { text: "Banks", value: "Banks" },
+  {
+    text: "Bars, Lounges and Alcohol Locations",
+    value: "Bars, Lounges and Alcohol Locations"
+  },
+  { text: "Beauty and Personal Care", value: "Beauty and Personal Care" },
+  { text: "Behavioral", value: "Behavioral" },
+  { text: "Big Box Stores", value: "Big Box Stores" },
+  { text: "Bookstores", value: "Bookstores" },
+  { text: "Brokerage and Investment", value: "Brokerage and Investment" },
+  { text: "Business and Finance", value: "Business and Finance" },
+  {
+    text: "Cafes, Coffee and Tea Houses",
+    value: "Cafes, Coffee and Tea Houses"
+  },
+  { text: "Car Dealerships", value: "Car Dealerships" },
+  {
+    text: "Car Maintenance, Parts and Repair",
+    value: "Car Maintenance, Parts and Repair"
+  },
+  { text: "Car and Truck Rentals", value: "Car and Truck Rentals" },
+  { text: "Casual Restaurants", value: "Casual Restaurants" },
+  {
+    text: "Clothing, Shoes and Accessories",
+    value: "Clothing, Shoes and Accessories"
+  },
+  { text: "Coffee and Tea", value: "Coffee and Tea" },
+  { text: "Convenience Stores", value: "Convenience Stores" },
+  { text: "Computers and Electronics", value: "Computers and Electronics" },
+  { text: "Cuisine Type", value: "Cuisine Type" },
+  { text: "Department Stores", value: "Department Stores" },
+  {
+    text: "Department Stores and Big Box Stores",
+    value: "Department Stores and Big Box Stores"
+  },
+  {
+    text: "Dollar Stores and Discount Stores",
+    value: "Dollar Stores and Discount Stores"
+  },
+  { text: "Education", value: "Education" },
+  {
+    text: "Electronics, Devices and Cell Phones",
+    value: "Electronics, Devices and Cell Phones"
+  },
+  { text: "Events and Event Planning", value: "Events and Event Planning" },
+  {
+    text: "Eyewear, Glasses and Sunglasses",
+    value: "Eyewear, Glasses and Sunglasses"
+  },
+  { text: "Fashion", value: "Fashion" },
+  { text: "Fast Casual Restaurants", value: "Fast Casual Restaurants" },
+  {
+    text: "Fast Food and QSR Diners (Quick Serve Restaurant Diner)",
+    value: "Fast Food and QSR Diners (Quick Serve Restaurant Diner)"
+  },
+  { text: "Financial Services", value: "Financial Services" },
+  { text: "Food and Dining", value: "Food and Dining" },
+  { text: "Food and Beverage", value: "Food and Beverage" },
+  { text: "Furniture and Home Decor", value: "Furniture and Home Decor" },
+  { text: "Games and Gaming", value: "Games and Gaming" },
+  { text: "Gas Stations", value: "Gas Stations" },
+  { text: "Gender", value: "Gender" },
+  { text: "Groceries", value: "Groceries" },
+  {
+    text: "Grocery Stores and Supermarkets",
+    value: "Grocery Stores and Supermarkets"
+  },
+  { text: "Gyms and Fitness Centers", value: "Gyms and Fitness Centers" },
+  { text: "Hair, Skin and Nails", value: "Hair, Skin and Nails" },
+  { text: "Health and Fitness", value: "Health and Fitness" },
+  { text: "Home Goods", value: "Home Goods" },
+  { text: "Home Improvement", value: "Home Improvement" },
+  { text: "Hotels and Motels", value: "Hotels and Motels" },
+  { text: "Household Income (HHI)", value: "Household Income (HHI)" },
+  { text: "Insurance", value: "Insurance" },
+  { text: "Jewelry and Watches", value: "Jewelry and Watches" },
+  { text: "Landmarks", value: "Landmarks" },
+  { text: "Legal", value: "Legal" },
+  { text: "Lifestyle and Lifestage", value: "Lifestyle and Lifestage" },
+  { text: "Lodging", value: "Lodging" },
+  { text: "Mattresses", value: "Mattresses" },
+  { text: "Media and Entertainment", value: "Media and Entertainment" },
+  {
+    text: "Mobile Devices and Cell Phones",
+    value: "Mobile Devices and Cell Phones"
+  },
+  { text: "Movers", value: "Movers" },
+  { text: "Movie Theaters and Theatres", value: "Movie Theaters and Theatres" },
+  { text: "Moving Services and Trucks", value: "Moving Services and Trucks" },
+  { text: "Office Supplies", value: "Office Supplies" },
+  { text: "Outdoor Recreation", value: "Outdoor Recreation" },
+  { text: "Outdoors and Sporting Goods", value: "Outdoors and Sporting Goods" },
+  { text: "Paint", value: "Paint" },
+  { text: "Parental Status", value: "Parental Status" },
+  { text: "Party Supplies", value: "Party Supplies" },
+  { text: "Pet Food and Pet Care", value: "Pet Food and Pet Care" },
+  { text: "Pharmacy (Drug Stores)", value: "Pharmacy (Drug Stores)" },
+  {
+    text: "Printing and Shipping (Couriers)",
+    value: "Printing and Shipping (Couriers)"
+  },
+  {
+    text: "Quick Serve, QSR and Fast Food",
+    value: "Quick Serve, QSR and Fast Food"
+  },
+  { text: "Real Estate", value: "Real Estate" },
+  { text: "Real Estate Agents", value: "Real Estate Agents" },
+  { text: "Restaurants", value: "Restaurants" },
+  { text: "Retail", value: "Retail" },
+  { text: "Skin Care and Body Care", value: "Skin Care and Body Care" },
+  {
+    text: "Supermarkets and Grocery Stores (Groceries)",
+    value: "Supermarkets and Grocery Stores (Groceries)"
+  },
+  { text: "Tax Preparation", value: "Tax Preparation" },
+  { text: "Technology", value: "Technology" },
+  { text: "Tourism", value: "Tourism" },
+  { text: "Transportation", value: "Transportation" },
+  { text: "Transportation Hubs", value: "Transportation Hubs" },
+  { text: "Travel", value: "Travel" },
+  { text: "US Congressional Districts", value: "US Congressional Districts" },
+  { text: "Used Car Dealerships", value: "Used Car Dealerships" },
+  { text: "Vitamins and Supplements", value: "Vitamins and Supplements" },
+  { text: "Warehouse Club Stores", value: "Warehouse Club Stores" },
+  {
+    text: "Weight Loss and Weight Management",
+    value: "Weight Loss and Weight Management"
+  }
+];
+
+const countries = [
+  { text: "US", value: "US" },
+  { text: "Australia", value: "Australia" },
+  { text: "Hong Kong", value: "Hong Kong" },
+  { text: "Japan", value: "Japan" },
+  { text: "Singapore", value: "Singapore" }
+];
 const verticals = [
   { text: "Automotive", value: "Automotive" },
   { text: "Business & Finance", value: "Business & Finance" },
@@ -44,7 +180,11 @@ const verticals = [
   { text: "Demographic", value: "Demographic" },
   { text: "Political", value: "Political" }
 ];
-const platforms = ["The Trade Desk", "Google", "Centro"];
+const platforms = [
+  { text: "Centro", value: "centro" },
+  { text: "Google", value: "google_id" },
+  { text: "The Trade Desk", value: "ttd" }
+];
 
 class App extends React.Component {
   rowSelection = () => ({
@@ -88,10 +228,21 @@ class App extends React.Component {
 
     const columns = [
       {
+        title: "ID",
+        dataIndex: "segment_id",
+        key: "segment_id",
+        width: 80,
+        sorter: (a, b) => a.segment_id.length - b.segment_id.length,
+        defaultSortOrder: "ascend",
+        render: text => <Text code>{text}</Text>
+      },
+      {
         title: "Country",
         dataIndex: "country",
         key: "country",
         width: 100,
+        filters: countries,
+        onFilter: (value, record) => record.country.indexOf(value) === 0,
         render: text => (
           <Tag>
             <Text strong>{text}</Text>
@@ -99,15 +250,23 @@ class App extends React.Component {
         )
       },
       {
-        title: "Segment",
-        dataIndex: "name",
-        key: "name",
+        title: "",
+        dataIndex: "description",
+        key: "description",
+        width: 30,
         render: (text, record) => (
           <Tooltip title={record.description}>
-            <Text>{text}</Text>
+            <Icon type="info-circle" />
           </Tooltip>
         )
       },
+      {
+        title: "Segment",
+        dataIndex: "name",
+        key: "name",
+        render: (text, record) => <Text strong>{text}</Text>
+      },
+
       {
         title: "Vertical",
         dataIndex: "vertical",
@@ -115,13 +274,15 @@ class App extends React.Component {
         width: 200,
         filters: verticals,
         onFilter: (value, record) => record.vertical.indexOf(value) === 0,
-        render: (text, record) => <Text strong>{text}</Text>
+        render: (text, record) => <Text>{text}</Text>
       },
       {
         title: "Tags",
         dataIndex: "tags",
         key: "tags",
         width: 200,
+        filters: tags,
+        onFilter: (value, record) => record.tags.indexOf(value) === 0,
         render: (tags, record) => (
           <>
             {tags.map((tag, i) => (
@@ -135,20 +296,56 @@ class App extends React.Component {
         dataIndex: "reach",
         key: "reach",
         width: 200,
-        sorter: (a, b) => a.reach - b.reach
+        sorter: (a, b) => a.reach - b.reach,
+        render: num => num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
       },
       {
         title: "Platform",
         dataIndex: "platform_ids",
         key: "platform_ids",
-        width: 300,
+        width: 200,
         render: (platform, record) => {
-          return platform.google_id ? (
-            <Tooltip title={platform.google_id}>
-              <Tag color="green">Google</Tag>
-            </Tooltip>
-          ) : (
-            undefined
+          return (
+            <>
+              {platform.google_id && (
+                <Tooltip
+                  title={
+                    <Text copyable style={{ color: "white" }}>
+                      {platform.google_id}
+                    </Text>
+                  }
+                >
+                  <Tag color="green">Google</Tag>
+                  <br />
+                </Tooltip>
+              )}
+
+              {platform.centro && (
+                <Tooltip
+                  title={
+                    <Text copyable style={{ color: "white" }}>
+                      {platform.centro}
+                    </Text>
+                  }
+                >
+                  <Tag color="orange">Centro</Tag>
+                  <br />
+                </Tooltip>
+              )}
+
+              {platform.ttd && (
+                <Tooltip
+                  title={
+                    <Text copyable style={{ color: "white" }}>
+                      {platform.ttd}
+                    </Text>
+                  }
+                >
+                  <Tag color="blue">The Trade Desk</Tag>
+                  <br />
+                </Tooltip>
+              )}
+            </>
           );
         }
       }
@@ -164,23 +361,21 @@ class App extends React.Component {
 
               <Row>
                 <Col span={4}>
-                  <div style={{ marginTop: 16, marginBottom: 16 }}>
-                    <Search
-                      placeholder="Search via name, ID, etc"
-                      onSearch={value => console.log(value)}
-                      style={{ width: 400 }}
-                    />
-                  </div>
+                  <Search
+                    placeholder="Search via name, ID, etc"
+                    onSearch={value => console.log(value)}
+                    style={{ width: 400 }}
+                  />
                 </Col>
               </Row>
 
               <Table
                 size="middle"
-                bordered
-                rowSelection={this.rowSelection()}
+                //bordered
+                //rowSelection={this.rowSelection()}
                 columns={columns}
                 dataSource={segments}
-                pagination={{ position: "top" }}
+                pagination={{ pageSize: 20, position: "both" }}
               />
             </Content>
           </Layout>

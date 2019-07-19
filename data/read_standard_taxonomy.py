d067761d983beb5b1dc66b8016b23344ad1ee554
Michael Vertuli <vertuli@factual.com>
2019-07-18 18:17:37 -0700

    Included path; search based on path.

--- data/read_standard_taxonomy.py
+++ data/read_standard_taxonomy.py
@@ -5,26 +5,20 @@ COUNTRIES = ['Japan', 'Australia', 'Hong Kong', 'Singapore']
 INPUT_PATH_1 = "Taxonomy for Hackathon.xlsx"
 INPUT_PATH_2 = "taxonomy_structure.tsv"
 OUTPUT_PATH = "../src/segments.json"
-
-
-def get_segment_tags() -> pd.DataFrame:
-    raw_df = pd.read_csv(INPUT_PATH_2, sep='\t')
-    clean_df = raw_df[['Targeting Code', 'Level3']]
-    clean_df = clean_df.rename(
-        columns={'Targeting Code': 'segment_id', 'Level3': 'tag'})
-    col_map = {
-        'Brand Shoppers': 'Chain',
-        'Place Category Visitors': 'Category',
-        'Behavioral - Food and Beverage': 'Behavioral',
-        'Behavioral - Lifestyle and Lifestage': 'Behavioral',
-        'Behavioral - Seasonal': 'Behavioral',
-        'Behavioral - Travel and Transportation': 'Behavioral',
-        'Behavioral - Auto': 'Behavioral',
-        'Behavioral - Retail': 'Behavioral',
-    }
-    clean_df['tag'] = clean_df['tag'].replace(col_map)
-    clean_df = clean_df.set_index('segment_id')
-    return clean_df
+SKIPPED_TAGS = ['Behavioral']
+FOCUS_REPLACEMENTS = {
+    'Brand Shoppers': 'Chain',
+    'Political': 'Political',
+    'Demographic': 'Demographic',
+    'Place Category Visitors': 'Place Category',
+    'Behavioral': 'Behavioral',
+    'Behavioral - Food and Beverage': 'Behavioral',
+    'Behavioral - Seasonal': 'Behavioral',
+    'Behavioral - Lifestyle and Lifestage': 'Behavioral',
+    'Behavioral - Auto': 'Behavioral',
+    'Behavioral - Travel and Transportation': 'Behavioral',
+    'Behavioral - Retail': 'Behavioral',
+}
 
 
 def get_segment_taxonomy() -> pd.DataFrame:
@@ -32,51 +26,45 @@ def get_segment_taxonomy() -> pd.DataFrame:
     google_df = read_standard_taxonomy_reach_google(dfs['Google'])
     centro_df = read_standard_taxonomy_reach_centro(dfs['Centro'])
     ttd_df = read_standard_taxonomy_reach_ttd(dfs['TTD'])
-    all_df = google_df.join(centro_df, how='outer')
-    all_df = all_df.join(ttd_df, how='outer')
-    all_platform_ids = pd.Series(name='platform_ids')
-    for segment_id, row in all_df.iterrows():
-        platform_ids = {}
-        platform_ids.update(row['platform_id_google'])
-        platform_ids.update(row['platform_id_centro'])
-        platform_ids.update(row['platform_id_ttd'])
-        all_platform_ids[segment_id] = platform_ids
-    all_df['platform_ids'] = all_platform_ids
-    all_df = all_df.drop(
-        columns=['platform_id_google', 'platform_id_centro', 'platform_id_ttd'])
-    mask = all_df['vertical'] == 'Political'
-    all_df.loc[mask, 'region'] = all_df.loc[mask, 'tags'].apply(lambda x: x[1])
-    all_df.loc[mask, 'tags'] = all_df.loc[mask, 'tags'].apply(lambda x: [x[0]])
-    all_df.loc[mask, 'tags'] = all_df.loc[mask]
+    all_df = google_df.join([centro_df, ttd_df])
+    all_df['platform_ids'] = all_df.apply(
+        lambda row: {'google_id': row['google_id'], 'centro_id': row['centro_id'],
+                     'ttd_id': row['ttd_id']}, axis=1)
+    all_df = all_df.drop(columns=['google_id', 'centro_id', 'ttd_id'])
+
+    focus_df = get_segment_focus()
+    all_df = all_df.join(focus_df)
     return all_df
 
 
 def read_standard_taxonomy_reach_google(raw_df: pd.DataFrame) -> pd.DataFrame:
     clean_df = pd.DataFrame()
     clean_df['segment_id'] = raw_df['Factual Segment ID']
-    clean_df['platform_id_google'] = raw_df['Google Segment ID'].apply(
-        lambda x: {'google_id': str(x)})
-    clean_df['name'] = raw_df['Segment Name'].apply(extract_name)
-    clean_df['country'] = raw_df['Segment Name'].apply(extract_country)
-    clean_df['vertical'] = raw_df['Segment Name'].apply(extract_vertical)
-    clean_df['description'] = raw_df.apply(extract_description, axis=1)
-    clean_df['reach'] = raw_df['Reach (Jul 2019)']
+    clean_df['google_id'] = raw_df['Google Segment ID']
+    tags = raw_df['Segment Name'].str.split('>')
+    tags = tags.apply(lambda vals: [val.strip().replace('&', 'and') for val in
+                                    vals] if vals else None)
+    tags = tags.apply(lambda vals: [val for val in vals if val not in SKIPPED_TAGS])
+    clean_df['name'] = tags.apply(lambda vals: vals[-1])
+    clean_df['country'] = tags.apply(
+        lambda vals: vals[1] if vals[1] in COUNTRIES else 'US')
+    clean_df['vertical'] = tags.apply(
+        lambda vals: vals[2] if vals[1] in COUNTRIES else vals[1])
+    clean_df['path'] = tags.apply(
+        lambda vals: " > ".join(vals[2:]) if vals[1] in COUNTRIES else " > ".join(
+            vals[1:]))
+    clean_df['reach'] = raw_df['Reach (Jul 2019)'].apply(int)
+    clean_df['description'] = raw_df['Description']
     clean_df['last_updated'] = date.today()
     clean_df = clean_df.set_index('segment_id')
     return clean_df
-
-
-def extract_description(row: pd.Series) -> str:
-    description = row['Description'] + '\n' + extract_tags(row['Segment Name'])
-    return description
 
 
 def read_standard_taxonomy_reach_centro(raw_df: pd.DataFrame) -> pd.DataFrame:
     raw_df = raw_df.loc[raw_df['Targetable']]
     clean_df = pd.DataFrame()
     clean_df['segment_id'] = raw_df['Factual ID']
-    clean_df['platform_id_centro'] = raw_df['Factual ID'].apply(
-        lambda x: {'centro': str(x)})
+    clean_df['centro_id'] = raw_df['Factual ID']
     clean_df['parent'] = raw_df['Factual Parent ID']
     clean_df = clean_df.set_index('segment_id')
     return clean_df
@@ -86,46 +74,23 @@ def read_standard_taxonomy_reach_ttd(raw_df: pd.DataFrame) -> pd.DataFrame:
     raw_df = raw_df.loc[raw_df['Buyable']]
     clean_df = pd.DataFrame()
     clean_df['segment_id'] = raw_df['ID']
-    clean_df['platform_id_ttd'] = raw_df['ID'].apply(lambda x: {'ttd': str(x)})
+    clean_df['ttd_id'] = raw_df['ID']
     clean_df = clean_df.set_index('segment_id')
     return clean_df
 
 
-def extract_name(path: str) -> str:
-    name = path.split('>')[-1].strip()
-    return name
-
-
-def extract_vertical(path: str) -> str:
-    tag = path.split('>')[1].strip()
-    if tag in COUNTRIES:
-        vertical = path.split('>')[2].strip()
-    else:
-        vertical = tag
-    return vertical
-
-
-def extract_country(path: str) -> str:
-    tag = path.split('>')[1].strip()
-    if tag in COUNTRIES:
-        country = tag
-    else:
-        country = 'US'
-    return country
-
-
-def extract_tags(path: str) -> str:
-    if extract_country(path) == 'US':
-        tags = path.split('>')[2:-1]
-    else:
-        tags = path.split('>')[3:-1]
-    tags = [tag.strip().replace('&', 'and') for tag in tags]
-    clean_tags = " > ".join(tags)
-    return clean_tags
+def get_segment_focus() -> pd.DataFrame:
+    focus_df = pd.read_csv(INPUT_PATH_2, sep='\t', usecols=['Targeting Code', 'Level3'])
+    focus_df = focus_df.rename(
+        columns={'Targeting Code': 'segment_id', 'Level3': 'focus'})
+    focus_df = focus_df.loc[focus_df['focus'] != 'DEPRECATED']
+    focus_df = focus_df.set_index('segment_id')
+    focus_df['focus'] = focus_df['focus'].replace(FOCUS_REPLACEMENTS)
+    return focus_df
 
 
 if __name__ == '__main__':
-    taxonomy_df = get_segment_taxonomy()
-    tags_df = get_segment_tags()
-    segments_df = taxonomy_df.join(tags_df)
+    segments_df = get_segment_taxonomy()
+    segments_df = segments_df.rename(columns={'focus': 'tags'})
+    segments_df['tags'] = segments_df['tags'].apply(lambda x: [x])
     segments_df.reset_index().to_json(OUTPUT_PATH, orient='records')
--- src/App.js
+++ src/App.js
@@ -20,134 +20,11 @@ const { Header, Content, Footer } = Layout;
 let segments = require("./segments.json");
 
 const tags = [
-  { text: "Advertising and Marketing", value: "Advertising and Marketing" },
-  { text: "Age", value: "Age" },
-  { text: "Amusement and Theme Parks", value: "Amusement and Theme Parks" },
-  { text: "Arts, Crafts and Fabric", value: "Arts, Crafts and Fabric" },
-  { text: "Baby and Children's Goods", value: "Baby and Children's Goods" },
-  { text: "Banks", value: "Banks" },
-  {
-    text: "Bars, Lounges and Alcohol Locations",
-    value: "Bars, Lounges and Alcohol Locations"
-  },
-  { text: "Beauty and Personal Care", value: "Beauty and Personal Care" },
+  { text: "Chain", value: "Chain" },
+  { text: "Political", value: "Political" },
+  { text: "Demographic", value: "Demographic" },
+  { text: "Place Category", value: "Place Category" },
   { text: "Behavioral", value: "Behavioral" },
-  { text: "Big Box Stores", value: "Big Box Stores" },
-  { text: "Bookstores", value: "Bookstores" },
-  { text: "Brokerage and Investment", value: "Brokerage and Investment" },
-  {
-    text: "Cafes, Coffee and Tea Houses",
-    value: "Cafes, Coffee and Tea Houses"
-  },
-  { text: "Car Dealerships", value: "Car Dealerships" },
-  {
-    text: "Car Maintenance, Parts and Repair",
-    value: "Car Maintenance, Parts and Repair"
-  },
-  { text: "Car and Truck Rentals", value: "Car and Truck Rentals" },
-  { text: "Casual Restaurants", value: "Casual Restaurants" },
-  {
-    text: "Clothing, Shoes and Accessories",
-    value: "Clothing, Shoes and Accessories"
-  },
-  { text: "Coffee and Tea", value: "Coffee and Tea" },
-  { text: "Convenience Stores", value: "Convenience Stores" },
-  { text: "Computers and Electronics", value: "Computers and Electronics" },
-  { text: "Cuisine Type", value: "Cuisine Type" },
-  { text: "Department Stores", value: "Department Stores" },
-  {
-    text: "Department Stores and Big Box Stores",
-    value: "Department Stores and Big Box Stores"
-  },
-  {
-    text: "Dollar Stores and Discount Stores",
-    value: "Dollar Stores and Discount Stores"
-  },
-  { text: "Education", value: "Education" },
-  {
-    text: "Electronics, Devices and Cell Phones",
-    value: "Electronics, Devices and Cell Phones"
-  },
-  { text: "Events and Event Planning", value: "Events and Event Planning" },
-  {
-    text: "Eyewear, Glasses and Sunglasses",
-    value: "Eyewear, Glasses and Sunglasses"
-  },
-  { text: "Fashion", value: "Fashion" },
-  { text: "Fast Casual Restaurants", value: "Fast Casual Restaurants" },
-  {
-    text: "Fast Food and QSR Diners (Quick Serve Restaurant Diner)",
-    value: "Fast Food and QSR Diners (Quick Serve Restaurant Diner)"
-  },
-  { text: "Financial Services", value: "Financial Services" },
-  { text: "Food and Beverage", value: "Food and Beverage" },
-  { text: "Furniture and Home Decor", value: "Furniture and Home Decor" },
-  { text: "Games and Gaming", value: "Games and Gaming" },
-  { text: "Gas Stations", value: "Gas Stations" },
-  { text: "Gender", value: "Gender" },
-  { text: "Groceries", value: "Groceries" },
-  {
-    text: "Grocery Stores and Supermarkets",
-    value: "Grocery Stores and Supermarkets"
-  },
-  { text: "Gyms and Fitness Centers", value: "Gyms and Fitness Centers" },
-  { text: "Hair, Skin and Nails", value: "Hair, Skin and Nails" },
-  { text: "Health and Fitness", value: "Health and Fitness" },
-  { text: "Home Goods", value: "Home Goods" },
-  { text: "Home Improvement", value: "Home Improvement" },
-  { text: "Hotels and Motels", value: "Hotels and Motels" },
-  { text: "Household Income (HHI)", value: "Household Income (HHI)" },
-  { text: "Insurance", value: "Insurance" },
-  { text: "Jewelry and Watches", value: "Jewelry and Watches" },
-  { text: "Landmarks", value: "Landmarks" },
-  { text: "Legal", value: "Legal" },
-  { text: "Lodging", value: "Lodging" },
-  { text: "Mattresses", value: "Mattresses" },
-  { text: "Media and Entertainment", value: "Media and Entertainment" },
-  {
-    text: "Mobile Devices and Cell Phones",
-    value: "Mobile Devices and Cell Phones"
-  },
-  { text: "Movers", value: "Movers" },
-  { text: "Movie Theaters and Theatres", value: "Movie Theaters and Theatres" },
-  { text: "Moving Services and Trucks", value: "Moving Services and Trucks" },
-  { text: "Office Supplies", value: "Office Supplies" },
-  { text: "Outdoor Recreation", value: "Outdoor Recreation" },
-  { text: "Outdoors and Sporting Goods", value: "Outdoors and Sporting Goods" },
-  { text: "Paint", value: "Paint" },
-  { text: "Parental Status", value: "Parental Status" },
-  { text: "Party Supplies", value: "Party Supplies" },
-  { text: "Pet Food and Pet Care", value: "Pet Food and Pet Care" },
-  { text: "Pharmacy (Drug Stores)", value: "Pharmacy (Drug Stores)" },
-  {
-    text: "Printing and Shipping (Couriers)",
-    value: "Printing and Shipping (Couriers)"
-  },
-  {
-    text: "Quick Serve, QSR and Fast Food",
-    value: "Quick Serve, QSR and Fast Food"
-  },
-  { text: "Real Estate", value: "Real Estate" },
-  { text: "Real Estate Agents", value: "Real Estate Agents" },
-  { text: "Restaurants", value: "Restaurants" },
-  { text: "Skin Care and Body Care", value: "Skin Care and Body Care" },
-  {
-    text: "Supermarkets and Grocery Stores (Groceries)",
-    value: "Supermarkets and Grocery Stores (Groceries)"
-  },
-  { text: "Tax Preparation", value: "Tax Preparation" },
-  { text: "Technology", value: "Technology" },
-  { text: "Tourism", value: "Tourism" },
-  { text: "Transportation", value: "Transportation" },
-  { text: "Transportation Hubs", value: "Transportation Hubs" },
-  { text: "US Congressional Districts", value: "US Congressional Districts" },
-  { text: "Used Car Dealerships", value: "Used Car Dealerships" },
-  { text: "Vitamins and Supplements", value: "Vitamins and Supplements" },
-  { text: "Warehouse Club Stores", value: "Warehouse Club Stores" },
-  {
-    text: "Weight Loss and Weight Management",
-    value: "Weight Loss and Weight Management"
-  }
 ];
 
 const countries = [
@@ -364,10 +241,12 @@ class App extends React.Component {
           <>
             <Text strong>{text}</Text>
             <br />
-            <Text type="secondary">{record.description}</Text>
+            <Text>{record.description}</Text>
+            <br />
+            <Text type="secondary">{record.path}</Text>
           </>
         ),
-        ...this.getColumnSearchProps("name")
+        ...this.getColumnSearchProps("path")
       },
       {
         title: "Reach",
@@ -406,7 +285,7 @@ class App extends React.Component {
         render: text => <Text strong>{text}</Text>
       },
       {
-        title: "Tags",
+        title: "Design",
         dataIndex: "tags",
         key: "tags",
         filteredValue: searchParams.get("tags")
@@ -452,19 +331,19 @@ class App extends React.Component {
                 </>
               )}
 
-              {platform.centro && (
+              {platform.centro_id && (
                 <>
                   <Tag color="orange">
-                    Centro: <Text copyable>{platform.centro}</Text>
+                    Centro: <Text copyable>{platform.centro_id}</Text>
                   </Tag>
                   <br />
                 </>
               )}
 
-              {platform.ttd && (
+              {platform.ttd_id && (
                 <>
                   <Tag color="blue">
-                    TTD: <Text copyable>{platform.ttd}</Text>
+                    TTD: <Text copyable>{platform.ttd_id}</Text>
                   </Tag>
                   <br />
                 </>
